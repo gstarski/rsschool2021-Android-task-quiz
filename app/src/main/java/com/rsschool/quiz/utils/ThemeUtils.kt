1b7b8ccce93ea4097fa8c0cc0a132a038c27cbbe
G.Starski <41999764+gstarski@users.noreply.github.com>
2021-06-19 08:10:54 +0300

    Add more themes

--- app/src/main/java/com/rsschool/quiz/utils/ThemeUtils.kt
+++ app/src/main/java/com/rsschool/quiz/utils/ThemeUtils.kt
@@ -6,4 +6,8 @@ fun getThemeCycled(n: Int) = arrayOf(
     R.style.Theme_Quiz_First,
     R.style.Theme_Quiz_Second,
     R.style.Theme_Quiz_Third,
+    R.style.Theme_Quiz_Fourth,
+    R.style.Theme_Quiz_Fifth,
+    R.style.Theme_Quiz_Sixth,
+    R.style.Theme_Quiz_Seventh,
 ).run { get(n % size) }
